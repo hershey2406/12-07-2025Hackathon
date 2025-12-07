3fbe4b2fc29ee2c05539cf7859b8758a180b13e6
hershey2406 <79227281+hershey2406@users.noreply.github.com>
2025-12-07 14:41:19 -0500

    Database Update

---
+++ backend/models.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Today(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(10), unique=True, nullable=False)
+
+    def to_dict(self):
+        return {"id": self.id, "date": self.date}
